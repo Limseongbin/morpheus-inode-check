bb8741f827231af8a019761d4b0a15ba95e42334
Limseongbin <lim900309@naver.com>
2022-07-22 16:04:41 +0900

    Update gathering.sh

--- roles/inode/files/gathering.sh
+++ roles/inode/files/gathering.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+limit=$1
+
 df -i | while read line; do
    filesystem=`echo $line | awk '{ print $1 }'`
    percent=`echo $line | awk '{ print $5 }' | sed "s/%//g"` 
    mountPoint=`echo $line | awk '{ print $6 }'`
    
-   if [ "${filesystem}" != "Filesystem" ] && [ ${percent} -ge 1 ]; then
+   if [ "${filesystem}" != "Filesystem" ] && [ ${percent} -ge ${limit} ]; then
       echo "${filesystem},${mountPoint},${percent}"
    fi
 done
